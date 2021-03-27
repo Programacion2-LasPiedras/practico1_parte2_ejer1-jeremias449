7dbcc7c8651a1afbcf0a866b443a9503fcbf0fa5
Jeremias  Erba <jeremiaserba7894@gmail.com>
2021-03-27 19:22:00 +0000

    Primer codigo

--- Programa.java
+++ Programa.java
@@ -1,24 +1,63 @@
 import java.util.Scanner;
 
 public class Programa {
-	
-	 //Definición de las variables para cargar los datos
-	 
+
+    String apellido;
+    int edad;
+    double altura;
+    long cedula;
+
 	 Scanner entrada=new Scanner(System.in);
 	
 	 void iniciar(){
-				
-		cargar();
-		imprimir();
-	
+		cargar(apellido,edad,altura,cedula);
 	}
-//Escribir aquí el método cargar
-//Este método deberá cargar desde el teclado los datos necesarios
+  void cargar(String dato1,int dato2,double dato3,long dato4){
+    System.out.println("Ingrese los datos pedidos a continuación");
+    System.out.println("");
+    dato1=apellido();
+    dato2=edad();
+    dato3=altura();
+    dato4=cedula();
+    imprimir(dato1, dato2, dato3, dato4);
+  }
+  String apellido(){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Cual es su apellido:");
+    String a =sc.nextLine();
+    return a;
+
+  }
+  int edad(){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("");
+    System.out.println("Cual es su edad:");
+    int b = sc.nextInt();
+    return b;
 
 
-//Escribir aquí el método imprimir
-//Este método deberá mostrar en pantalla  los datos cargados
+  }
 
-
-
+  double altura(){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("");
+    System.out.println("Cual es su altura:");    //Esta altura es en metros, separando la unidad con un punto
+    double c = sc.nextDouble();
+    return c;
+  }
+  long cedula(){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("");
+    System.out.println("Cual es su Cédula:");
+    long b = sc.nextLong();
+    return b;
+  }
+  void imprimir(String a, int b, double c, long d){
+    System.out.println("Los datos que ingreso: ");
+    System.out.println("");
+    System.out.println("su apellido: " + a);
+    System.out.println("su edad: "+ b);
+    System.out.println("su altura: " + c);
+    System.out.println("su cédula: " + d);
+  }
 }
